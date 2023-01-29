c70006d08f14f1fcb6d69c47517a10651b564844
Taehun Kim <kth3321@gmail.com>
2023-01-29 23:31:36 +0900

    Update Copyright

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -35,7 +35,7 @@ export const FooterImpl: React.FC = () => {
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>Copyright 2022 {config.author}</div>
+      <div className={styles.copyright}>Copyright ⓒ 2022-2023 {config.author}. All Rights Reserved.</div>
 
       <div className={styles.settings}>
         {hasMounted && (
