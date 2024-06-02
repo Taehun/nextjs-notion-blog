01bcd594d2275a18223c9c9c7829359e61a47f5f
Taehun Kim <kth3321@gmail.com>
2024-06-02 23:44:59 +0900

    Change the vector DB library interface

--- lib/vector-db.ts
+++ lib/vector-db.ts
@@ -1,13 +1,20 @@
 import * as lancedb from '@lancedb/lancedb'
 import { getEnv } from './get-config-value'
 
-export async function getVectorDB() {
+export async function getVectorTable() {
   const dbURI = getEnv('S3_VECTOR_DB_URI')
-  const db = await lancedb.connect(dbURI, {
+  const conn = await lancedb.connect(dbURI, {
     storageOptions: {
       awsAccessKeyId: getEnv('AWS_ACCESS_KEY_ID'),
       awsSecretAccessKey: getEnv('AWS_SECRET_ACCESS_KEY')
     }
   })
-  return db
+  const table = await conn.openTable('blog-vector')
+  return table
 }
+
+const apiKey = getEnv('OPENAI_API_KEY')
+export const embedding = new lancedb.embedding.OpenAIEmbeddingFunction(
+  apiKey,
+  'text-embedding-3-small'
+)
