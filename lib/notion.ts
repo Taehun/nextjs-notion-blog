20cc2a92db60b043e5e28e28fda4aedc555b7e73
Taehun Kim <kth3321@gmail.com>
2024-06-10 01:16:35 +0900

    Extract text contents by notion record map

--- lib/notion.ts
+++ lib/notion.ts
@@ -37,6 +37,54 @@ const getNavigationLinkPages = pMemoize(
   }
 )
 
+// 텍스트 블록 타입 정의
+const textTypes = new Set([
+  'text',
+  'header',
+  'sub_header',
+  'sub_sub_header',
+  'bulleted_list',
+  'numbered_list',
+  'to_do',
+  'toggle',
+  'quote'
+])
+
+// `recordMap`에서 텍스트 블록을 추출하는 함수
+function extractTextFromRecordMap(recordMap: any): string {
+  let contents = ''
+
+  // `block` 속성에서 모든 블록을 가져옵니다
+  const blocks = Object.values(recordMap.block).map(
+    (blockObj: any) => blockObj.value
+  )
+
+  blocks.forEach((block: any) => {
+    const blockType = block.type
+
+    if (textTypes.has(blockType)) {
+      const baseContent = block.properties?.title?.join('') + '\n'
+      let content = ''
+
+      if (blockType.startsWith('header')) {
+        // 제목 블록은 한 줄 공백을 추가
+        content = contents.length > 0 ? '\n' + baseContent : baseContent
+      } else if (blockType === 'code') {
+        // 코드 블록은 앞뒤로 ``` 문자를 추가
+        const lang = block[blockType]?.language || ''
+        content = `\`\`\`${lang}\n${baseContent}\`\`\`\n`
+      } else if (blockType.endsWith('list')) {
+        // List item 블록은 앞에 '- ' 문자 추가
+        content = '- ' + baseContent
+      } else {
+        content = baseContent
+      }
+      contents += content
+    }
+  })
+  return contents
+}
+
 export async function getPage(pageId: string): Promise<ExtendedRecordMap> {
   let recordMap = await notion.getPage(pageId)
 
@@ -59,6 +107,8 @@ export async function getPage(pageId: string): Promise<ExtendedRecordMap> {
     const previewImageMap = await getPreviewImageMap(recordMap)
     ;(recordMap as any).preview_images = previewImageMap
   }
+  const textContent = extractTextFromRecordMap(recordMap)
+  console.log(textContent)
 
   return recordMap
 }
